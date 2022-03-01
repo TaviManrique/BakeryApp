071071c52961fd3b77863aa81d4c6e2c237d2ead
Tavi <tavidanner96@gmail.com>
2022-02-28 20:48:12 -0500

    LazyRow Card Promotions

---
+++ app/src/main/java/com/manriquetavi/bakeryapp/presentation/components/PromotionsLazyRow.kt
@@ -0,0 +1,107 @@
+package com.manriquetavi.bakeryapp.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.manriquetavi.bakeryapp.R
+
+@Composable
+fun PromotionsLazyRow() {
+    LazyRow(
+        modifier = Modifier.height(160.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            PromotionItem(
+                imagePainter = painterResource(id = R.drawable.promotion),
+                title = "Fruit",
+                subtitle = "Start @",
+                header = "$1",
+                backgroundColor = MaterialTheme.colors.primary
+            )
+        }
+
+        item {
+            PromotionItem(
+                imagePainter = painterResource(id = R.drawable.promotion),
+                title = "Meat",
+                subtitle = "Discount",
+                header = "20%",
+                backgroundColor = Color(0xff6EB6F5)
+            )
+        }
+    }
+}
+
+@Composable
+fun PromotionItem(
+    title: String = "",
+    subtitle: String = "",
+    header: String = "",
+    backgroundColor: Color = Color.Transparent,
+    imagePainter: Painter
+) {
+    Card(
+        modifier = Modifier.width(300.dp),
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = backgroundColor,
+        elevation = 0.dp
+    ) {
+        Row {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = subtitle,
+                    fontSize = 16.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = header,
+                    fontSize = 28.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Image(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                painter = imagePainter, contentDescription = "",
+                alignment = Alignment.CenterEnd,
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PromotionsLazyRowPreview() {
+    PromotionsLazyRow()
+}
